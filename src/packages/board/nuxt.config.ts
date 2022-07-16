ca183a88ee86190ddbaceaa1ab2dd9136ade5b98
kentarominami <kentaro.minami@yourmystar.jp>
2022-07-16 17:06:37 +0900

    todo追加

--- src/packages/board/nuxt.config.ts
+++ src/packages/board/nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    // @todo Importing directly from a nuxt.config file is not allowed. Instead, use runtime config or a module. [importing /nuxt.config.ts from index.html]  のエラーが出ているので後で修正
     buildModules: ['@nuxtjs/tailwindcss'],
     css: ['@/assets/css/tailwind.css'],
 })
