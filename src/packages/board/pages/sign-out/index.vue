3fe73764ef624280800c5a15f77d7f397db8b2d5
kentarominami <kentaro.minami@yourmystar.jp>
2022-07-17 12:08:50 +0900

    sign-out処理追加

---
+++ src/packages/board/pages/sign-out/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {useSignOut} from '~/composables/auth'
+
+const {signOut} = useSignOut(() => {
+  const url = '/sign-in'
+  window.location.href = url.toString()
+})
+
+await signOut()
+</script>
+<template>
+  <div>
+  </div>
+</template>
